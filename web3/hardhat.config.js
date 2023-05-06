27523a0da7d1984f0055c0e613043f70fb8a7f8b
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-05-06 10:54:42 +0600

    deployed to test net

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+  },
 };
